8062577960ea94232a27a927e9d6712adf51b186
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-08-07 18:21:32 -0700

    Some typos and forgot password confirmation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,7 +2,10 @@ class Follow < ActiveRecord::Base
 
   attr_accessible :follower, :follower_id, :following, :following_id
 
+  # The person following
   belongs_to :follower, class_name: "User"
+
+  # The person being followed
   belongs_to :following, , class_name: "User"
 
 end
