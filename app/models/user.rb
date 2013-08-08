b6dec66a4f8704133b0151f920e371dd04f45a55
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-08-07 18:39:45 -0700

    Added profile picture and pictures to users... The default profile picture still needs to be set

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
 
   has_many :follows
 
+  has_one :profile_pic, as: :imageable, :class_name => 'Image', dependent: :destroy
+  accepts_nested_attributes_for :profile_pic
+  attr_accessible :profile_pic_attributes
+
+  has_many :images, dependent: :destroy
+
   validates :username, 
     presence: true,
     uniqueness: true
@@ -39,6 +45,15 @@ class User < ActiveRecord::Base
   # This overwrites default getters
   def following
     Follow.where("follower = ?", self.id)
+  end
+
+  # This return their profile picture. Or a default in none are set. 
+  def get_image
+    if profile_pic
+      return profile_pic.url.thumb.to_s
+    else
+      return "/assets/default.jpeg"
+    end
   end
 
 end
