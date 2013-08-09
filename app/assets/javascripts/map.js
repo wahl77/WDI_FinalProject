6741ab2933b7c63c929404f139284562fc1fe227
tomgalpin <tomgalpin@gmail.com>
2013-08-08 22:15:27 -0700

    initial map functionality

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var map = L.mapbox.map('map_container', 'examples.map-uci7ul8p', { zoomControl: false })
+      .setView([37.7, -122.4183], 12);
+
+  // moves the zoon controls the right side; defaults to 'topleft' if this line is left off.
+  new L.Control.Zoom({ position: 'topright' }).addTo(map);
+});
+
+
