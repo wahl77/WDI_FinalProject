337a58632ee53dc7e226c67fd0b99e25c093dc68
Zara Peisker <zarapeisker@gmail.com>
2013-08-08 23:22:05 -0700

    Twitter OAuth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130809005534) do
+ActiveRecord::Schema.define(:version => 20130809043820) do
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.string   "provider",   :null => false
+    t.string   "uid",        :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "follows", :force => true do |t|
     t.integer  "follower_id"
