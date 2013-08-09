b4f36158fcb829796b9cc8bf483eb73ea02b0bdd
Christina <christina.v.stewart@gmail.com>
2013-08-08 21:07:50 -0700

    started user#show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ RailsProject::Application.routes.draw do
   root :to => 'static_pages#index'
 
   resources :sessions, only:[:new, :create, :destroy]
+
+  get "profile/:id" => 'users#show', :as => "profile"
 end
