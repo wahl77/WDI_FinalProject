a2a0d5dc745e52597e7e4367823f217f01717374
Zara Peisker <zarapeisker@gmail.com>
2013-08-08 23:22:05 -0700

    Twitter OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsProject::Application.routes.draw do
+
+  match "oauth/callback" => "oauths#callback"
+  match "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+
   root :to => 'static_pages#index'
 
   resources :sessions, only:[:new, :create, :destroy]
