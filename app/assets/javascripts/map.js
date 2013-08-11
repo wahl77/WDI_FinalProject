9847c0091435c144039e260553e91ca8d580f4d5
tomgalpin <tomgalpin@gmail.com>
2013-08-11 14:21:37 -0700

    styled login and signup forms, plus fonts throughout homepage

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -10,9 +10,6 @@ $(document).ready(function() {
   // defines the map and the 'type' of map.  Here is where we can change the look of the map
   var map = L.mapbox.map('map_container', 'examples.map-uci7ul8p', { zoomControl: false });
 
-  // // sets the map to this lat/long, with a zoom as the third argument
-  // map.setView([37.7572, -122.3999], 13);
-
   // moves the zoom controls the right side; defaults to 'topleft' if this line is left off.
   new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
