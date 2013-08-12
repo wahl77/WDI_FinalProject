64e4fb755fec8ac1c1390446e33778a24aa2230d
tomgalpin <tomgalpin@gmail.com>
2013-08-12 13:08:29 -0700

    working on profie page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  var geoJson;
   // toggles the class on the filter buttons so that when they are clicked they look 'depressed', similarly when they are unclicked
   var depress_button = function() {
     $('.filter_buttons').click(function() {
@@ -8,6 +9,12 @@ $(document).ready(function() {
   };
   depress_button();
 
+  // defines the map and the 'type' of map.  Here is where we can change the look of the map
+  var map = L.mapbox.map('map_container', 'examples.map-uci7ul8p', { zoomControl: false });
+
+  // moves the zoom controls the right side; defaults to 'topleft' if this line is left off.
+  new L.Control.Zoom({ position: 'topright' }).addTo(map);
+
   function geolocate() {
     if (navigator.geolocation) {
       console.log(navigator.geolocation);
@@ -15,16 +22,14 @@ $(document).ready(function() {
     }
   }
 
-  // defines the map and the 'type' of map.  Here is where we can change the look of the map
-  var map = L.mapbox.map('map_container', 'examples.map-uci7ul8p', { zoomControl: false });
+  geolocate();
 
-  // moves the zoom controls the right side; defaults to 'topleft' if this line is left off.
-  new L.Control.Zoom({ position: 'topright' }).addTo(map);
+
 
   // centers and sets the map on the user's geolocation
   map.on('locationfound', function(e) {
       map.fitBounds(e.bounds);
-      map.setView([e.latlng.lat, e.latlng.lng], 12);
+      map.setView([e.latlng.lat, e.latlng.lng], 15);
   });
 
   //  This example uses jQuery to make selecting items in the slideshow easier.
@@ -64,34 +69,67 @@ $(document).ready(function() {
 
   var marker = [];
 
-  var add_markers = function(response) {
-    for (var i = 0; i <= json_response.length; i++)
-      marker.push({
-        type: 'Feature',
-        "geometry": {
-          "type": "Point",
-          // of note:  for some reason that lat/long need to be reversed here such that it is long/lat
-          "coordinates": [json_response[i].long, json_response[i].lat]
-        },
-        "properties": {
-          'title': 'When I... I realized...',
-          'icon': {
-            "iconUrl": json_response[i].url[0],
-            "iconSize": [50, 50], // size of the icon
-            "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
-            "popupAnchor": [0, -25]  // point from which the popup should open relative to the iconAnchor
-          },
-        'images': [
-          [json_response[i].url, json_response[i].caption]
-        ]
+  var add_markers = function(json_response) {
+    console.log('hello');
+    console.log(json_response);
+
+      for (var i = 0; i <= json_response.length; i++) {
+          marker.push({
+            type: 'Feature',
+            "geometry": {
+              "type": "Point",
+              // of note:  for some reason that lat/long need to be reversed here such that it is long/lat
+              "coordinates": [json_response[i].long, json_response[i].lat]
+            },
+            "properties": {
+              'title': 'When I... I realized...',
+              'icon': {
+                "iconUrl": json_response[i][0].url[0],
+                "iconSize": [50, 50], // size of the icon
+                "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+                "popupAnchor": [0, -25]  // point from which the popup should open relative to the iconAnchor
+              },
+            'images': [
+              [json_response[i].url, json_response[i][0].caption]
+            ]
+          }
+        }); // close geoJson/marker.push
+          console.log(marker);
       }
-    }); // close geoJson/marker.push
   };
+
+
 
   var geoJson = {
     type: 'FeatureCollection',
     features: marker
   };
+
+  var geoJson2 = [{
+        type: 'Feature',
+        "geometry": {
+          "type": "Point",
+          // of note:  for some reason that lat/long need to be reversed here such that it is long/lat
+          "coordinates": [-122.3999, 37.7572]
+        },
+        // This is for a custom marker on the map
+        "properties": {
+          'title': 'little kitten',
+          'icon': {
+            "iconUrl": "http://placekitten.com/50/50",
+            "iconSize": [50, 50], // size of the icon
+            "iconAnchor": [25, 25], // point of the icon which will correspond to marker's location
+            "popupAnchor": [0, -25]  // point from which the popup should open relative to the iconAnchor
+          },
+          // These are the images for the photo display
+          // Store the image url and caption in an array
+          'images': [
+            ['http://i.imgur.com/O6QEpBs.jpg','Baby The U.S. Capitol after the burning of Washington during the War of 1812'],
+            ['http://i.imgur.com/xND1MND.jpg','Ford\'s Theatre in the 19th century, site of the 1865 assassination of President Lincoln'],
+            ['http://i.imgur.com/EKJmqui.jpg','TheNational Cherry Blossom Festival is celebrated around the city each spring.']
+          ]
+        }
+    }]; // close kitten geoJson2
 
   // delays the show of each marker (created above)
   // var delay = 0;
@@ -137,7 +175,7 @@ $(document).ready(function() {
           '<a href="#" class="prev" >&laquo; Previous</a>' +
           '<a href="#" class="next" >Next &raquo;</a>' +
         '</div>' +
-        '<button id="search_insights"> Search Similar Insights </button>'
+        '<button id="search_insights"> Search Similar Insights </button>' +
       '</div>';
 
 
@@ -154,12 +192,12 @@ $(document).ready(function() {
 
   // Add features to the map and sets each geoJson object
   map.markerLayer.setGeoJSON(geoJson);
-
+  map.markerLayer.setGeoJSON(geoJson2);
   // because 'prev' & 'next'/popup do not exist in the DOM yet, moveSlide doesn't work without a call on the body afterward.
   $('body').on('click', '.prev', moveSlide);
   $('body').on('click', '.next', moveSlide);
 
-  geolocate();
+
 
 
 });
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,9 +3,9 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
   belongs_to :user
 
-  mount_uploader :url, ImageUploader
+  # mount_uploader :url, ImageUploader
 
-  after_create :extract_exif_info
+  # after_create :extract_exif_info
 
 
   # Define what in a picture is searchable. For the time being,
@@ -26,31 +26,31 @@ class Image < ActiveRecord::Base
   # The following method gets the GPS data from the picture if any and stores it alongside
   # the picture for any furter usage. This happens only after the picture saved as not
   # all pictures contain the information.
-  def extract_exif_info
-    img = Magick::Image.read(File.join(Rails.root, "public", self.url.url))[0]
+  # def extract_exif_info
+  #   img = Magick::Image.read(File.join(Rails.root, "public", self.url.url))[0]
 
-    return unless img
+  #   return unless img
 
-    img_lat = img.get_exif_by_entry('GPSLatitude')[0][1].split(', ') rescue nil
-    img_lng = img.get_exif_by_entry('GPSLongitude')[0][1].split(', ') rescue nil
+  #   img_lat = img.get_exif_by_entry('GPSLatitude')[0][1].split(', ') rescue nil
+  #   img_lng = img.get_exif_by_entry('GPSLongitude')[0][1].split(', ') rescue nil
 
-    lat_ref = img.get_exif_by_entry('GPSLatitudeRef')[0][1] rescue nil
-    lng_ref = img.get_exif_by_entry('GPSLongitudeRef')[0][1] rescue nil
+  #   lat_ref = img.get_exif_by_entry('GPSLatitudeRef')[0][1] rescue nil
+  #   lng_ref = img.get_exif_by_entry('GPSLongitudeRef')[0][1] rescue nil
 
-    return unless img_lat && img_lng && lat_ref && lng_ref
+  #   return unless img_lat && img_lng && lat_ref && lng_ref
 
-    latitude = to_frac(img_lat[0]) + (to_frac(img_lat[1])/60) + (to_frac(img_lat[2])/3600)
-    longitude = to_frac(img_lng[0]) + (to_frac(img_lng[1])/60) + (to_frac(img_lng[2])/3600)
+  #   latitude = to_frac(img_lat[0]) + (to_frac(img_lat[1])/60) + (to_frac(img_lat[2])/3600)
+  #   longitude = to_frac(img_lng[0]) + (to_frac(img_lng[1])/60) + (to_frac(img_lng[2])/3600)
 
-    latitude = latitude * -1 if lat_ref == 'S'  # (N is +, S is -)
-    longitude = longitude * -1 if lng_ref == 'W'   # (W is -, E is +)
+  #   latitude = latitude * -1 if lat_ref == 'S'  # (N is +, S is -)
+  #   longitude = longitude * -1 if lng_ref == 'W'   # (W is -, E is +)
 
 
-    self.lat = latitude
-    self.long = longitude
+  #   self.lat = latitude
+  #   self.long = longitude
 
-    self.save
-  end
+  #   self.save
+  # end
 
   def to_frac(strng)
     numerator, denominator = strng.split('/').map(&:to_f)
