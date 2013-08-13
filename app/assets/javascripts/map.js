feb159567aa3c3b4009167eafff6e1f59f649991
Zara Peisker <zarapeisker@gmail.com>
2013-08-12 18:30:56 -0700

    polished up markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -135,7 +135,7 @@ $(document).ready(function(){
           },
           // These are the images for the photo display
           // Store the image url and caption in an array
-          "images": [this['dataset']['image'], this['dataset']['caption']]
+          "images": [[this['dataset']['image'], this['dataset']['caption']]]
         }, //close properties
       }); // close push
     }); //close you_images function
@@ -210,13 +210,14 @@ $(document).ready(function(){
       }
 
       // Create custom popup content
+      // commented out previous next divs in case we want to integrate in the future
       var popupContent =
         '<div id="' + feature.properties.id + '" class="popup">' +
           '<h2>' + feature.properties.title + '</h2>' +
           '<div class="slideshow">' + slideshowContent + '</div>' +
           '<div class="cycle">' +
-            '<a href="#" class="prev" >&laquo; Previous</a>' +
-            '<a href="#" class="next" >Next &raquo;</a>' +
+            // '<a href="#" class="prev" >&laquo; Previous</a>' +
+            // '<a href="#" class="next" >Next &raquo;</a>' +
           '</div>' +
         '</div>';
 
